3b09fe8ac4584504bbcdf9191247af2e301eccac
bumatik022 <bumatik022@student.wethinkcode.co.za>
2023-08-11 14:38:45 +0200

    updated typescript

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Message from "./Message"
+
+function App(){
+  return <div><Message /></div>
+}
+
+// export:can be used somehwere else
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+//PascalCasing always
+function Message(){
+    //JSX: JavaScript XML
+    return<h1>Hello World</h1>;
+}
+
+export default Message;
