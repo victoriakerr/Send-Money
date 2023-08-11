8128bd0a9efd123f217379532f9e58ae2463ba2f
zakerr022 <zakerr022@student.wethinkcode.co.za>
2023-08-11 17:02:04 +0200

    added the Remittance cost

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
-import Message from "./Message"
+import React, { useState, useEffect } from 'react';
+import CountryDropdown from './CountryDropdown';
+import ProductDropdown from './ProductDropdown';
 
-function App(){
-  return <div><Message /></div>
+function App() {
+  const [selectedCountry, setSelectedCountry] = useState<Country | null>(null);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const [remittanceCost, setRemittanceCost] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (selectedCountry && selectedProduct) {
+      async function calculateRemittanceCost() {
+        try {
+          const response = await fetch(
+            `https://api-uct.mukuru.com/taurus/v1/products/price-check?pay_in_country=${selectedCountry.iso2}&pay_out_country=${selectedProduct.destinationCountry.iso2}&pay_in_currency=CURRENCY1&pay_out_currency=CURRENCY2&type=${selectedProduct.id}`
+          );
+
+          const data = await response.json();
+          setRemittanceCost(data.pay_out_amount); // Replace with the actual property from the API response
+        } catch (error) {
+          console.error('Error calculating remittance cost:', error);
+        }
+      }
+
+      calculateRemittanceCost();
+    }
+  }, [selectedCountry, selectedProduct]);
+
+  return (
+    <div className="App">
+      <h1>Remittance Cost Calculator</h1>
+
+      <CountryDropdown onSelectCountry={setSelectedCountry} />
+      <ProductDropdown onSelectProduct={setSelectedProduct} />
+
+      {remittanceCost !== null && (
+        <div>
+          <h2>Remittance Cost</h2>
+          <p>{remittanceCost}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-// export:can be used somehwere else
 export default App;
